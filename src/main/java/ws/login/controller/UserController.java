c30d7f2acc88415490dfcd1075151ed6fcacf76b
violet-two <820918859@qq.com>
2022-07-21 15:02:29 +0800

    Initial commit

---
+++ src/main/java/ws/login/controller/UserController.java
@@ -0,0 +1,16 @@
+package ws.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public Object login(){
+        return "success";
+    }
+
+}
